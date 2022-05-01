b68bced414899cee493f9379865af78d0a21e7a6
LJH0716 <jhmk0805@naver.com>
2022-05-02 00:39:10 +0900

    2022-05-02 예외처리

--- src/com/javaex/ex01/LastNumberApp.java
+++ src/com/javaex/ex01/LastNumberApp.java
@@ -4,12 +4,10 @@ public class LastNumberApp {
 
 	public static void main(String[] args) {
 
-		// 수정하지 마세요
 		lastValue();
 	}
 
-
-	//런타임시 오류발생
+	// 런타임시 오류발생
 	public static void lastValue() {
 
 		int[] intArray = new int[3];
@@ -20,9 +18,12 @@ public class LastNumberApp {
 		int result;
 		
 		//아래부분을 try~catch문을 사용하여 수정하세요
-		result = intArray[3];
-		System.out.println(result);
-		
+		try {
+			result = intArray[3];
+			System.out.println(result);
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("배열의 범위를 넘었습니다.");
+		}
 	}
 
 }
